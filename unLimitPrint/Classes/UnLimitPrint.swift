b42f9c6085bc2be0acb43c842a56399d6acdcd9b
Natthaaek <natthaaek.f@gmail.com>
2017-09-29 18:09:39 +0700

    second push

---
+++ unLimitPrint/Classes/UnLimitPrint.swift
@@ -0,0 +1,17 @@
+//
+//  UnLimitPrint.swift
+//  Pods-unLimitPrint_Example
+//
+//  Created by Natthaaek Phoajinda on 9/29/2560 BE.
+//
+
+import UIKit
+class UnLimitKit{
+    func unLimitPrint(word:String){
+        var count : Int = 0
+        while true{
+            count =  count + 1
+            print("count :  \(count) : \(word)")
+        }
+    }
+}
